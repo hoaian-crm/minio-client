f14b01bc05f0f3911196e707a3023d1c789669bf
hackermasterlord <126839836+hackermasterlord@users.noreply.github.com>
2024-04-30 12:56:10 +0700

    Update cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 export const sync = async () => {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
   app.listen(process.env.APP_PORT);
 };
 
